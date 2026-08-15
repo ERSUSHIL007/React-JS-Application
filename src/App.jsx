fc3b46279ff7fbd20554c61df68dee5793a22aeb
Sushil Kumar <ersushilkr@rediffmail.com>
2026-08-15 14:48:14 +0530

    Add React application with Redux Todo management

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from "./components/AddTodo"
+import Todo from "./components/Todo"
+
+function App() {
+
+  return (
+    <>
+      <h1> React Redux Toolkit App</h1>
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
